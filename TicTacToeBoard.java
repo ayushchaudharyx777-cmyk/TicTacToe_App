810201ea12e86d15cead4213350d2a5a87077e02
Ayush Chaudary <ayush.chaudary@email.com>
2026-04-21 14:48:44 +0530

    Added UC1: Empty Tic Tac Toe Board

---
+++ TicTacToeBoard.java
@@ -0,0 +1,25 @@
+public class TicTacToeBoard {
+
+    public static void main(String[] args) {
+
+        // Create 3x3 board
+        char[][] board = new char[3][3];
+
+        // Initialize board with '-'
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                board[i][j] = '-';
+            }
+        }
+
+        // Print board
+        System.out.println("Empty Tic-Tac-Toe Board:\n");
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
